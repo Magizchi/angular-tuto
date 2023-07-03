bfb49d72e02f3097b9046cd22e8d5c53e2edf568
Magizchi <rganesalingam@webnet.fr>
2023-07-03 09:01:34 +0000

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<h1>Welcom to {{ title }}</h1>`,
+  // templateUrl: './app.component.html'
+  // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Pokemon-app';
 }
